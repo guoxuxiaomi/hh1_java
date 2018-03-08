d0158c473957949ddf4ff71a4cb87cfb2ed4e0c9
guoxu <guoxu1234>
2018-03-08 13:59:15 +0800

    cs

--- spring all/spring-boot hh/src/main/java/com/hh/HhStarter.java
+++ spring all/spring-boot hh/src/main/java/com/hh/HhStarter.java
@@ -8,14 +8,7 @@ public class HhStarter {
 
 
 
-    /**
-     * 除了在Eclipse中右键执行run，还可以使用命令：mvn spring-boot:run
-     *
-     * 运行之后在浏览器中输入：http://localhost:8080/。最后发现这两种的结果一样。
-     *
-     * @param args
-     * @throws Exception
-     */
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(HhStarter.class,args);
     }
